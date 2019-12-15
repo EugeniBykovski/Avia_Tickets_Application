2c870cc9a898d123214219258a6a9d5121696d06
EugeniBykovski <bykovskieugeni@mail.ru>
2019-12-15 14:44:09 +0300

    created application architecture, connected server

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Avia_Tickets_Application
 Airfare search application with loading of their price, country and air carrier.
+
+1. Possibility to choose departure country
+2. Ability to choose the country of arrival
+3. Ability to select departure date and arrival date
+4. Implemented the ability to change currencies
+5. Work is underway with the ES6 stack, OOP, AJAX, Webpack.
+
+We have a server. We need to build communication between our server and the client side.
+
+The first layer will be Services. With their help, we will organize interaction with the server (we have this API SERVICE). It will have a set of methods for receiving any data from the server and the client part (App JS) will access this service, which will ask it for this or that information for our application.
+
+The application also contains a settings file (Config), which contains all the application settings. It is connected to both the App JS and the API Service.
+
+The application also has some plugins, several of which are connected to the API Service and App JS. Required for AJAX requests, Promises, communication between the application and the server. 
+
+In order to fulfill the requests, we need the AXIOS plugin.
---
+++ src/services/apiService.js
@@ -0,0 +1,48 @@
+// Класс с набором методов для взаимодействия с сервером. (+ Axios)
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/*
+    Сейчас на сервере есть несколько endpoints:
+    1. countries - array of countries
+    2. cities - array of cities
+    3. prices - array of prices
+*/
+
+class Api {
+    constructor(config) {
+        // будет создавать свойство 
+        this.url = config.url;
+    }
+
+    // набор методов для работы с сервером (они предоставляются нашему app.js, из которого мы будем вызывать те методы, которые нам нужны)
+
+    // будем выполнять запрос на сервер
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch (err) {
+            console.log(err);
+            return Promise.reject(err);
+        }
+    }
+
+    // UI часть по выбору соответствующих параметров
+    async prices(params) {}
+}
+
+const api = new Api(config); // создаем экземпляр этого класса (передаем тот config, который мы создали)
+
+// создав api, мы его экспортируем
+export default api;
